7bbbe224d9ce90326ff8764fdac47750929a3b0a
Masik <maz@helenas-macbook-pro.local>
2018-12-07 10:48:24 -0600

    Initial Commit

---
+++ Groupify/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Groupify
+//
+//  Created by Masik on 12/7/18.
+//  Copyright © 2018 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
