93b2c05d81466ce6fce136258b3d0f9d6ee391e2
Masik <maz@helenas-macbook-pro.local>
2018-12-14 10:48:41 -0600

    initializer and instance variables of the student class

---
+++ Groupify/Class_AnyClass.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Groupify
+//
+//  Created by Masik on 12/13/18.
+//  Copyright © 2018 District196.org. All rights reserved.
+//
+
+import Foundation
+
+class AnyClass
+{
+    
+}
---
+++ Groupify/Class_Student.swift
@@ -0,0 +1,52 @@
+//
+//  Class_Student.swift
+//  Groupify
+//
+//  Created by Masik on 12/13/18.
+//  Copyright © 2018 District196.org. All rights reserved.
+//
+
+import Foundation
+
+class Student
+{
+    var firstName: String
+    var lastName: String
+    var grade: Double
+    var presence: Bool
+    //var personNumber: Int
+    
+    init(fName: String, lName: String, studentGrade: Double, studentPresence: Bool)
+    {
+        firstName = fName
+        lastName = lName
+        grade = studentGrade
+        presence = studentPresence
+    }
+    
+    
+    
+    
+}
+
+/*
+ Example code
+ class SurveyQuestion {
+ var text: String
+ var response: String?
+ init(text: String) {
+ self.text = text
+ }
+ func ask() {
+ print(text)
+ }
+ }
+ let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+ cheeseQuestion.ask()
+ // Prints "Do you like cheese?"
+ cheeseQuestion.response = "Yes, I do like cheese."
+ 
+ 
+ 
+ }
+ */
