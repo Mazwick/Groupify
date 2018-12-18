473c35ce534ee33d37e24c738e43ba74473b129f
Masik <maz@helenas-macbook-pro.local>
2018-12-18 11:06:22 -0600

    Empty array in classes

--- Groupify/Class_AnyClass.swift
+++ Groupify/Class_AnyClass.swift
@@ -10,5 +10,28 @@ import Foundation
 
 class AnyClass
 {
+    var className: String
+    var classSize: Int
+    var classList: [Student]
+    //var someInts = [Int]()
+    //someInts.count --> number of items in array
+    //2 items in array (index 0 and 1) count = 2
     
+ 
+ 
+    init(cName: String)
+    {
+        className = "New Class"
+        classList = [Student]()
+        classSize = classList.count
+        
+    }
+    func addStudent()
+    {
+        
+    }
+    func deleteStudent()
+    {
+        
+    }
 }
